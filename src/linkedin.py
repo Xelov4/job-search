774275e6a2b4a3ca8dd4b37e4c805eebaa41ac95
Hritik003 <hritik.raj@nutanix.com>
2025-01-31 21:52:04 +0530

    tool for searching through feed

--- src/linkedin.py
+++ src/linkedin.py
@@ -12,11 +12,34 @@ logger = logging.getLogger(__name__)
 def get_creds():
     return Linkedin(os.getenv("LINKEDIN_EMAIL"), os.getenv("LINKEDIN_PASSWORD"), debug=True)
 
-@mcp.tool
+@mcp.tool()
 def get_profile():
+    """
+    Retrieves the User Profile
+    """
     linkedin = get_creds()
     profile = linkedin.get_profile()
     return profile
+@mcp.tool()
+def get_feed_posts(limit: int = 10, offset: int = 0) -> str:
+    """
+    Retrieve LinkedIn feed posts.
+
+    :return: List of feed post details
+    """
+    linkedin = get_creds()
+    try:
+        post_urns = linkedin.get_feed_posts(limit=limit, offset=offset)
+    except Exception as e:
+        logger.error(f"Error: {e}")
+        return f"Error: {e}"
+    
+    posts = ""
+    for urn in post_urns:
+        posts += f"Post by {urn["author_name"]}: {urn["content"]}\n"
+
+    return posts
 
 if __name__ == "__main__":
-    mcp.run()
+    mcp.run(transport='stdio')
+    # get_feed_posts()
